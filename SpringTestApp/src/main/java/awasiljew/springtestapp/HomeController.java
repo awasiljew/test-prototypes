98ba9ac85fffd90d82466cd08aaa314b3e33ae03
awasiljew <awasiljew@gmail.com>
2013-03-14 13:37:20 +0100

    Initialized testing repo

---
+++ SpringTestApp/src/main/java/awasiljew/springtestapp/HomeController.java
@@ -0,0 +1,19 @@
+package awasiljew.springtestapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author adam
+ */
+@Controller
+public class HomeController {
+ 
+    @RequestMapping(value = "/")
+    public String home() {
+        System.out.println("HomeController: Passing through...");
+        return "home";
+    }
+    
+}
